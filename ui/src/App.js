2e18c9ed030a0803dcac7dc6b2f07e7e5d520d51
Theo <thoecastillo@yahoo.com>
2025-07-30 23:38:36 +0200

    Fetch cube data from API and pass it to RubiksCube component

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,11 +1,29 @@
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { cubeApi } from './api/api_method';
 import RubiksCube from './components/RubiksCube';
 
 function App() {
+  const [cubeData, setCubeData] = useState(null);
+
+  useEffect(() => {
+    const fetchCube = async () => {
+      try {
+        const data = await cubeApi.getCube();
+        setCubeData(data);
+      } catch (error) {
+        console.error('Erreur:', error.message);
+      }
+    };
+    fetchCube();
+  }, []);
+
+  useEffect(() => {
+    console.log(cubeData);
+  }, [cubeData]);
+
   return (
     <div className="App">
-      <RubiksCube />
+      <RubiksCube cubeData={cubeData} />
     </div>
   );
 }
