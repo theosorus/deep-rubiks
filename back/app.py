a0099e03771a15fdcf0abeac9e33ebddbda335a7
Theo <thoecastillo@yahoo.com>
2025-07-30 23:39:48 +0200

    Remove unused cube and move model files to streamline the codebase

--- back/app.py
+++
@@ -1,6 +0,0 @@
-from models.cube import Cube
-
-if __name__ == "__main__":
-    cube = Cube()
-    print("cube \n")
-
--- back/models/colors.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class Colors(Enum) : 
-    White = 1
-    Blue = 2
-    Red = 3
-    Yellow = 4
-    Green = 5
-    Orange = 6
