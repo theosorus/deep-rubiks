5618fdad67091ccd9efbba0bd51e81e4e5f4d14c
Theo <thoecastillo@yahoo.com>
2025-08-09 19:27:02 +0200

    Add API routes for available moves and cube rotation functionality

--- back/app/api/routes/cube.py
+++ back/app/api/routes/cube.py
@@ -2,6 +2,9 @@
 from fastapi.responses import JSONResponse
 from fastapi import APIRouter, Request
 
+from models.moves import NOTATION_TO_MOVE
+from models.move_request import MoveRequest
+
 
 cube_router = APIRouter()
 
@@ -12,3 +15,33 @@ async def get_cube(request: Request):
     """
     cube_json = request.app.state.cube.__dict__()
     return cube_json
+
+
+@cube_router.get("/moves")
+async def get_cube(request: Request):
+    moves = list(NOTATION_TO_MOVE.keys())
+    print(f"Available moves: {moves}")
+    return moves
+
+
+@cube_router.post("/rotate")
+async def make_move(request: Request, move_request: MoveRequest):
+    """
+    Make a move on the cube based on the notation
+    """
+    received_move = move_request.move
+    cube = request.app.state.cube
+    print(f"Received move: {received_move}")
+    move_class = NOTATION_TO_MOVE.get(received_move.upper())
+    print(f"Move class found: {move_class}")
+    
+    if not move_class:
+        return JSONResponse(status_code=400, content={"error": "Invalid move notation"})
+    
+    move_instance = move_class()
+    move_instance.make_move_on_cube(cube)
+    
+    return JSONResponse(
+        status_code=200,
+        content={
+            "message": f"Move {received_move} applied successfully"})
