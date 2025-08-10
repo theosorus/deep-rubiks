59f5b388d2f9cd68794bfea5222436d7d56e80f5
Theo <thoecastillo@yahoo.com>
2025-08-10 11:20:49 +0200

    add reset root

--- back/app/api/routes/cube.py
+++ back/app/api/routes/cube.py
@@ -41,7 +41,21 @@ async def make_move(request: Request, move_request: MoveRequest):
     move_instance = move_class()
     move_instance.make_move_on_cube(cube)
     
+    print(f"Cube state after move:")
+    print(cube)
+    
     return JSONResponse(
         status_code=200,
         content={
             "message": f"Move {received_move} applied successfully"})
+    
+    
+@cube_router.get("/reset")
+async def reset_cube(request: Request):
+    cube = request.app.state.cube
+    cube.reset_cube()
+    return JSONResponse(
+        status_code=200,
+        content={
+            "message": "Cube reset successfully",
+        })
