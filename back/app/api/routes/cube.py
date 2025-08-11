beff3bc3af4112eaffc996db05eb9a8df26928ce
Theo <thoecastillo@yahoo.com>
2025-08-11 13:12:20 +0200

    Refactor cube routes to use AVAILABLE_MOVES and MOVE_CLASSES for move handling

--- back/app/api/routes/cube.py
+++ back/app/api/routes/cube.py
@@ -2,7 +2,7 @@
 from fastapi.responses import JSONResponse
 from fastapi import APIRouter, Request
 
-from models.moves import NOTATION_TO_MOVE
+from models.moves import AVAILABLE_MOVES,MOVE_CLASSES
 from models.move_request import MoveRequest
 
 
@@ -14,14 +14,14 @@ async def get_cube(request: Request):
     Get the current state of the cube
     """
     cube_json = request.app.state.cube.__dict__()
+    print(cube_json)
     return cube_json
 
 
 @cube_router.get("/moves")
 async def get_cube(request: Request):
-    moves = list(NOTATION_TO_MOVE.keys())
-    print(f"Available moves: {moves}")
-    return moves
+    print(f"Available moves: {AVAILABLE_MOVES}")
+    return AVAILABLE_MOVES
 
 
 @cube_router.post("/rotate")
@@ -32,7 +32,7 @@ async def make_move(request: Request, move_request: MoveRequest):
     received_move = move_request.move
     cube = request.app.state.cube
     print(f"Received move: {received_move}")
-    move_class = NOTATION_TO_MOVE.get(received_move.upper())
+    move_class = MOVE_CLASSES.get(received_move.upper())
     print(f"Move class found: {move_class}")
     
     if not move_class:
@@ -54,6 +54,7 @@ async def make_move(request: Request, move_request: MoveRequest):
 async def reset_cube(request: Request):
     cube = request.app.state.cube
     cube.reset_cube()
+    print(cube)
     return JSONResponse(
         status_code=200,
         content={
