21bc318a365702f53c0b368d312dcc864a74876f
Theo <thoecastillo@yahoo.com>
2025-08-11 13:12:31 +0200

    Refactor color definitions to use dictionaries instead of Enum for better flexibility

--- back/app/models/colors.py
+++ back/app/models/colors.py
@@ -1,10 +1,30 @@
-from enum import Enum
 
 
-class Colors(Enum) : 
-    white = 1
-    blue = 2
-    red = 3
-    yellow = 4
-    green = 5
-    orange = 6
+COLORS = {
+    "white": 0,
+    "blue": 1,
+    "red": 2,
+    "yellow": 3,
+    "green": 4,
+    "orange": 5
+}
+
+INT_TO_COLORS = {v: k for k, v in COLORS.items()}
+    
+DEFAULT_FACE_ORDER = ["U", "F", "B", "R", "L", "D"]
+FACE_INDEX = {face: idx for idx, face in enumerate(DEFAULT_FACE_ORDER)}
+
+DEFAULT_FACE_COLORS = {
+    "U": "white",
+    "F": "blue",
+    "B": "red",
+    "R": "yellow",
+    "L": "green",
+    "D": "orange"
+}
+
+COLORS_TO_INT = {color: idx for idx, color in enumerate(DEFAULT_FACE_COLORS.values())}
+COLORS_TO_NAME = {idx: color for color, idx in COLORS_TO_INT.items()}
+
+
+
