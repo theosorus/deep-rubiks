9696cf40e661a1f0f0a74e14a1592d143f974e17
Theo <thoecastillo@yahoo.com>
2025-08-15 16:59:04 +0200

    Refactor color mappings and update face color assignments for consistency

--- back/app/models/colors.py
+++ back/app/models/colors.py
@@ -1,29 +1,23 @@
 
 
-COLORS = {
-    "white": 0,
-    "blue": 1,
-    "red": 2,
-    "yellow": 3,
-    "green": 4,
-    "orange": 5
-}
 
-INT_TO_COLORS = {v: k for k, v in COLORS.items()}
-    
+
+
 DEFAULT_FACE_ORDER = ["U", "F", "B", "R", "L", "D"]
 FACE_INDEX = {face: idx for idx, face in enumerate(DEFAULT_FACE_ORDER)}
 
 DEFAULT_FACE_COLORS = {
     "U": "white",
-    "F": "blue",
-    "B": "red",
-    "R": "yellow",
-    "L": "green",
-    "D": "orange"
+    "F": "green",
+    "B": "blue",
+    "R": "red",
+    "L": "orange",
+    "D": "yellow"
 }
 
-COLORS_TO_INT = {color: idx for idx, color in enumerate(DEFAULT_FACE_COLORS.values())}
+COLORS = {name: idx for idx, name in enumerate(DEFAULT_FACE_COLORS.values())}
+INT_TO_COLORS = {v: k for k, v in COLORS.items()}
+COLORS_TO_INT = COLORS
 COLORS_TO_NAME = {idx: color for color, idx in COLORS_TO_INT.items()}
 
 
